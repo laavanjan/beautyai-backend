ce9ebc5c8cfc6b54787268d5661c2c173fc08b22
primedevintern10 <primedevintern10@gmail.com>
2026-03-01 23:19:18 +0530

    initial commit

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 load_dotenv()
 
-REDIS_URL = os.getenv("REDIS_URL")
+
 LLM_PROVIDER = os.getenv("LLM_PROVIDER", "groq")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 MODEL = os.getenv("MODEL", "groq-2.0-pro")
